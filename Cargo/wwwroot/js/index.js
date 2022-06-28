a45446f98fab6b6a1f25e62e02c49b178774e50a
Сергей Евсеев <webaspentity@outlook.com>
2022-06-28 23:46:04 +0300

    28.06.22 23:45

--- Cargo/wwwroot/js/index.js
+++ Cargo/wwwroot/js/index.js
@@ -1,34 +1,42 @@
 ﻿const menu = document.querySelector('.header__menu-group');
 const burger = document.querySelector('.burger');
+let lock = false;
 
-window.addEventListener('resize', function (event) {
+
+window.addEventListener('resize', function () {
     const menu = document.querySelector('.header__menu-group');
     const burger = document.querySelector('.burger');
     let width = this.document.body.clientWidth;
     if (width > 768 && menu.classList.contains('open')) {
         menu.classList.remove('open');
         burger.classList.remove('active');
-        ScrollUnlock();
+        ScrollUnlock("event-resize-unlock");
     }
 }, true);
 
 function ScrollLock() {
-    let pagePosition = window.scrollY;
-    document.body.style.position = 'fixed';
-    document.body.dataset.position = pagePosition;
-    document.body.style.top = - pagePosition + 'px';
-    document.documentElement.style.scrollBehavior = 'auto';
+    if (!lock) {
+        let pagePosition = window.scrollY;
+        document.body.style.position = 'fixed';
+        lock = true;
+        document.body.dataset.position = pagePosition;
+        document.body.style.top = - pagePosition + 'px';
+        document.documentElement.style.scrollBehavior = 'auto';
+    }
 }
 
 function ScrollUnlock() {
-    let pagePosition = parseInt(document.body.dataset.position, 10);
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo({
-        top: pagePosition,
-        behavior: "auto"
-    });
-    document.documentElement.style.scrollBehavior = 'smooth';
+    if (lock) {
+        let pagePosition = parseInt(document.body.dataset.position, 10);
+        document.body.style.position = '';
+        document.body.style.top = '';
+        lock = false;
+        window.scrollTo({
+            top: pagePosition,
+            behavior: "auto"
+        });
+        document.documentElement.style.scrollBehavior = 'smooth';
+    }
 }
 
 function Scroll(id) {
