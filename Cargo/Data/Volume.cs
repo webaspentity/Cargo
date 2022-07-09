46d10d08d6c7d52e568a496028fcaedfde6ac765
Сергей Евсеев <webaspentity@outlook.com>
2022-07-09 09:35:41 +0300

    release

---
+++ Cargo/Data/Volume.cs
@@ -0,0 +1,17 @@
+﻿namespace Cargo.Data;
+using System.ComponentModel.DataAnnotations;
+
+public class Volume
+{
+    [Required]
+    public double Width { get; set; }
+    [Required]
+    public double Height { get; set; }
+    [Required]
+    public double Length { get; set; }
+    [DisplayFormat(DataFormatString = "{0:F2}")]
+    public double Value
+    {
+        get => Width * Height * Length;
+    }
+}
